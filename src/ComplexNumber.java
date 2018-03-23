1997232373c1fcae9ebe849b159d13ce36ec1985
Unknown <marketing.KevinConti@gmail.com>
2018-03-23 18:41:20 -0400

    Initial Commit
    Complex numbers class and tests completed

---
+++ src/ComplexNumber.java
@@ -0,0 +1,102 @@
+public class ComplexNumber {
+    private double zReal;
+    private double zImaginary;
+    private double modulus;
+    private double argument;
+
+    public ComplexNumber(double zReal, double zImaginary) {
+        this.zReal = zReal;
+        this.zImaginary = zImaginary;
+    }
+
+    //Instance Methods
+    public ComplexNumber add(ComplexNumber number) {
+        return new ComplexNumber(this.getzReal() + number.getzReal(), this.getzImaginary() + number.getzImaginary());
+    }
+
+    public ComplexNumber subtract(ComplexNumber number) {
+        return new ComplexNumber(this.getzReal() - number.getzReal(), this.getzImaginary() - number.getzImaginary());
+    }
+
+    public ComplexNumber multiply(ComplexNumber number) {
+        double ac = this.getzReal() * number.getzReal();
+        double bd = this.getzImaginary() * number.getzImaginary();
+        double ad = this.getzReal() * number.getzImaginary();
+        double bc = this.getzImaginary() * number.getzReal();
+        return new ComplexNumber((ac - bd), (ad + bc));
+    }
+
+    public ComplexNumber multiply(double number, boolean isImaginaryMultiplication){
+        ComplexNumber mult = this.multiply(number);
+        if(!isImaginaryMultiplication){
+            //Then this number is just a scalar, do scalar multiplication
+            return mult;
+        } else{
+            return new ComplexNumber(mult.getzImaginary() * -1, mult.getzReal());
+        }
+    }
+
+    public ComplexNumber multiply(double scalar){
+        return new ComplexNumber(this.getzReal() * scalar, this.getzImaginary() * scalar);
+    }
+
+    public ComplexNumber divide(ComplexNumber number) {
+        double ac = this.getzReal() * number.getzReal();
+        double bd = this.getzImaginary() * number.getzImaginary();
+        double ad = this.getzReal() * number.getzImaginary();
+        double bc = this.getzImaginary() * number.getzReal();
+        double real = (ac + bd) / (Math.pow(number.getzReal(), 2) + Math.pow(number.getzImaginary(), 2));
+        double imaginary = (bc - ad) / (Math.pow(number.getzReal(), 2) + Math.pow(number.getzImaginary(), 2));
+        return new ComplexNumber(real, imaginary);
+    }
+
+    private void updateModulus() {
+        this.setModulus(Math.sqrt(Math.pow(this.getzReal(), 2) + Math.pow(this.getzImaginary(), 2)));
+    }
+
+    //CONSTRUCTORS
+
+    private void updateArgument() {
+        double argument = Math.toDegrees(Math.atan(this.getzImaginary() / this.getzReal()));
+        if(this.getzImaginary() < 0){
+            argument *= -1;
+        }
+        this.setArgument(argument);
+    }
+
+    //GETTERS AND SETTERS
+
+    public double getzReal() {
+        return zReal;
+    }
+
+    public void setzReal(double zReal) {
+        this.zReal = zReal;
+    }
+
+    public double getzImaginary() {
+        return zImaginary;
+    }
+
+    public void setzImaginary(double zImaginary) {
+        this.zImaginary = zImaginary;
+    }
+
+    public double getModulus() {
+        updateModulus();
+        return modulus;
+    }
+
+    public void setModulus(double modulus) {
+        this.modulus = modulus;
+    }
+
+    public double getArgument() {
+        updateArgument();
+        return argument;
+    }
+
+    public void setArgument(double argument) {
+        this.argument = argument;
+    }
+}
---
+++ src/ComplexNumberTest.java
@@ -0,0 +1,85 @@
+import static org.junit.Assert.*;
+
+public class ComplexNumberTest {
+
+    @org.junit.Test
+    public void add() {
+        ComplexNumber one = new ComplexNumber(5, 2);
+        ComplexNumber two = new ComplexNumber(3, -7);
+
+        ComplexNumber myAnswer = one.add(two);
+        ComplexNumber actualAnswer = new ComplexNumber(8, -5);
+
+        assertEquals(myAnswer.getzReal(), actualAnswer.getzReal(), 0);
+        assertEquals(myAnswer.getzImaginary(), actualAnswer.getzImaginary(), 0);
+    }
+
+    @org.junit.Test
+    public void subtract() {
+        ComplexNumber one = new ComplexNumber(2, -3);
+        ComplexNumber two = new ComplexNumber(6, -18);
+
+        ComplexNumber myAnswer = one.subtract(two);
+        ComplexNumber actualAnswer = new ComplexNumber(-4, 15);
+
+        assertEquals(myAnswer.getzReal(), actualAnswer.getzReal(), 0);
+        assertEquals(myAnswer.getzImaginary(), actualAnswer.getzImaginary(), 0);
+    }
+
+    @org.junit.Test
+    public void multiply() {
+        //Test scalar multiplication
+        ComplexNumber one = new ComplexNumber(13, 5);
+        double scalar = -4;
+
+        ComplexNumber myAnswer = one.multiply(scalar);
+        ComplexNumber actualAnswer = new ComplexNumber(-52, -20);
+
+        assertEquals(myAnswer.getzReal(), actualAnswer.getzReal(), 0);
+        assertEquals(myAnswer.getzImaginary(), actualAnswer.getzImaginary(), 0);
+
+        //test imaginary number * complex number
+        double imaginary = 2;
+        one = new ComplexNumber(3, -8);
+
+        myAnswer = one.multiply(imaginary, true);
+        actualAnswer = new ComplexNumber(16, 6);
+
+        assertEquals(myAnswer.getzReal(), actualAnswer.getzReal(), 0);
+        assertEquals(myAnswer.getzImaginary(), actualAnswer.getzImaginary(), 0);
+
+        //Test complex number multiplication
+        ComplexNumber three = new ComplexNumber(1, 4);
+        ComplexNumber four = new ComplexNumber(5, 1);
+
+        myAnswer = three.multiply(four);
+        actualAnswer = new ComplexNumber(1, 21);
+
+        assertEquals(myAnswer.getzReal(), actualAnswer.getzReal(), 0);
+        assertEquals(myAnswer.getzImaginary(), actualAnswer.getzImaginary(), 0);
+    }
+
+    @org.junit.Test
+    public void divide() {
+        ComplexNumber one = new ComplexNumber(20, -4);
+        ComplexNumber two = new ComplexNumber(3, 2);
+
+        ComplexNumber myAnswer = one.divide(two);
+        ComplexNumber actualAnswer = new ComplexNumber(4, -4);
+
+        assertEquals(myAnswer.getzReal(), actualAnswer.getzReal(), 0);
+        assertEquals(myAnswer.getzImaginary(), actualAnswer.getzImaginary(), 0);
+    }
+
+    @org.junit.Test
+    public void getModulus() {
+        ComplexNumber test = new ComplexNumber(4, 3);
+        assertEquals(5, test.getModulus(), .001);
+    }
+
+    @org.junit.Test
+    public void getArgument() {
+        ComplexNumber test = new ComplexNumber(4, 3);
+        assertEquals(36.87, test.getArgument(), 0.001);
+    }
+}
